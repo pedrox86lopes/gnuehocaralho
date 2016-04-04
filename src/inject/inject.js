45d36ee86d87e7943b84299de002b04b24dbf5d2
Pedro Lopes <pedro.secinfo@gmail.com>
2016-04-04 17:18:08 -0300

    haha

---
+++ src/inject/inject.js
@@ -0,0 +1,24 @@
+(function() {
+    function htmlreplace(a, b, reflags, element) {    
+        if (!element) element = document.body;
+        var nodes = element.childNodes;
+        var r = new RegExp(a, reflags);
+        for (var n=0; n<nodes.length; n++) {
+            if (nodes[n].nodeType == Node.TEXT_NODE) {
+                nodes[n].textContent = nodes[n].textContent.replace(r, b);
+            } else {
+                htmlreplace(a, b, reflags, nodes[n]);
+            }
+        }
+    }
+
+    var readyStateCheckInterval = setInterval(function() {
+	    if (document.readyState === "complete") {
+		    clearInterval(readyStateCheckInterval);
+
+		    htmlreplace("GNU", "GNU EH O CARALHO!!!", 'g');
+		    htmlreplace("GNU[gG]", "GNU eh o caralho!!!", 'g');
+		    htmlreplace("ubuntu", "gnu eh o caralho porra!!!", 'gi');
+	    }
+    }, 10);
+})();
